30d6369855e3fcd91d681f93e86a65da0db47e01
Alex Scarborough <alex@teak.io>
2023-06-23 09:16:48 -0700

    Allow using AWS provider v5.

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.22, < 5"
+      version = ">= 4.22, < 6"
 
       configuration_aliases = [aws.meta]
     }
